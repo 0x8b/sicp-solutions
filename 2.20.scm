9e6562b5c86a935618058f57e4008cf50c08e000
Mariusz Gumienny <mkgumienny@gmail.com>
2020-06-22 21:43:22 +0200

    refactor: 2.20

--- 2.20.scm
+++ 2.20.scm
@@ -1,11 +1,10 @@
-(define (same-parity . list)
-  (let ((parity-of-first (remainder (car list) 2)))
-    (define (iter list)
-      (if (null? list) nil
-          (let ((head (car list))
-                (tail (cdr list)))
-            (if (= parity-of-first (remainder head 2))
-                (cons head (iter tail))
-                (iter tail)))))
+(define nil '())
 
-    (iter list)))
+(define (same-parity head . tail)
+  (define (iter items)
+    (if (null? items) nil
+      (if (even? (+ (car items) head))
+          (cons (car items) (iter (cdr items)))
+          (iter (cdr items)))))
+
+    (cons head (iter tail)))
